244991be5009c7802861b22d2d7265d5571230e5
sharonjenifer11 <sharonjenifer4@gmail.com>
2026-02-04 22:09:07 +0530

    Create OVERVIEW.m

---
+++ OVERVIEW.m
@@ -0,0 +1,18 @@
+# Frontend Mini Project
+
+This is a mini project website created using HTML, CSS, and JavaScript.
+
+## 📌 Features
+- Clean UI
+- Separate CSS and JS files
+- Multi-page website
+- Beginner-friendly structure
+
+## 🛠 Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## 👩‍💻 Author
+Sharon Jenifer
+
